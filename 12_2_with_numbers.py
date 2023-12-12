fc72cc726312ff34db1e0da5a7bbe031f2ef0206
Tobias Hofmann (rm2mw061244) <tobias.hofmann.th1@roche.com>
2023-12-12 09:31:16 +0100

    earlier break even

--- 12_2_with_numbers.py
+++ 12_2_with_numbers.py
@@ -1,6 +1,6 @@
 def possible(info,test):
   tmp = [True if a==b or a=='?' else False for (a,b) in zip(info,test)]
-  return all(tmp)  
+  return all(tmp)
 
 def split(x, n, info, blocks):
   if n == 1:
@@ -12,12 +12,12 @@ def split(x, n, info, blocks):
   for i in range(1,x+1):
     # Check if zeros possible
     if not possible(info[:i],'.'*i):
-      continue
-    
+      break
+
     # Check if ones possible
     if not possible(info[i:(i+blocks[0])],'#'*blocks[0]):
       continue
-    
+
     t = split(x-i,n-1, info[(i+blocks[0]):], blocks[1:])
     t2 = [[i] + j for j in t]
     ls += t2
@@ -28,7 +28,7 @@ def transform(x, blocks):
   t[::2] = ['.'*j for j in x]
   t[1::2] = ['#'*j for j in blocks]
   return ''.join(t)
-  
+
 
 infos = open('12_test').read().splitlines()
 
@@ -40,17 +40,17 @@ for line in infos:
   # unfold
   blocks = blocks + blocks + blocks + blocks + blocks
   info = info + '?' + info + '?' + info + '?' + info + '?' + info
-  
+
   info = '.' + info + '.'
-  
+
   l = len(info)
   nwhites = l - sum(blocks)
-      
+
   combs = split(nwhites, len(blocks)+1, info, blocks)
-  
+
   #S = [transform(i,blocks) for i in combs]
-  
+
   print(len(combs))
   ss += len(combs)
-  
+
 print('ss:',ss)
